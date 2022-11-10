e806da3975f0c66eec7acd629a9231bae0bdef23
ulusalfaktoring <115483530+ulusalfaktoring@users.noreply.github.com>
2022-11-11 01:44:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
 
+app.get("/test-request", (req, res) => {
+  res.send("work");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
